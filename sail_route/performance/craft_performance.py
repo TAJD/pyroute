2b193c1b8ad97d4c8e48ce2815074d4e4b2516ee
TAJD <tom0dickson@gmail.com>
2018-06-19 14:49:37 +0100

    working tests for performance functions

--- sail_route/performance/craft_performance.py
+++ sail_route/performance/craft_performance.py
@@ -1,4 +1,4 @@
-""" Craft performanceself.
+""" Craft performance.
 
 Thomas Dickson
 thomas.dickson@soton.ac.uk
@@ -7,12 +7,6 @@ thomas.dickson@soton.ac.uk
 import numpy as np
 from scipy.interpolate import interp2d
 from numba import jit
-
-
-def get_perf():
-    path = "/Users/thomasdickson/Documents/sail_routing/routing/data/first_40_farr.csv"
-    perf = np.genfromtxt(path, delimiter=";", skip_header=1)
-    return perf[:, 1:]
 
 
 class polar(object):
@@ -42,18 +36,3 @@ class polar(object):
         p = interp2d(self.twa_range, self.tws_range, self.perf,
                      kind='linear')
         return p(twa, tws)*self.unc
-
-
-def return_boat_perf():
-    perf = get_perf()
-    twa = np.array([30.0, 36.0, 42.0, 50.0, 70.0, 90.0,
-                    120.0, 130.0, 150.0, 160.0, 180.0])
-    tws = np.array([4.0, 6.0, 8.0, 10.0, 12.0, 14.0,
-                    16.0, 20.0, 25.0, 30.0, 35.0])
-    first_40 = polar(twa, tws, perf)
-    return first_40
-
-
-if __name__ == '__main__':
-    boat = return_boat_perf()
-    print(boat.return_perf(30.0, 4.0))
